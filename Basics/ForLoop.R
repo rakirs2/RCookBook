8ce69cc8cb27f79630830726954c00b1e3140707
Srikar <srikarmylavarapu@gmail.com>
2025-11-05 22:40:59 -0600

    working ish with kmers

---
+++ Basics/ForLoop.R
@@ -0,0 +1,5 @@
+sequence <- 1:5
+
+for (variable in sequence) {
+  print(variable)
+}
--- Kmers/BasicKmerCounter.R
+++ Kmers/BasicKmerCounter.R
@@ -1,27 +1,30 @@
+rm(list = ls())
 # Install Biostrings if not already installed
 if (!requireNamespace("BiocManager", quietly = TRUE))
-install.packages("BiocManager")
+  install.packages("BiocManager")
 BiocManager::install("Biostrings")
 library(Biostrings)
 
+# Might need to change wd to the data folder.
 current_wd <- getwd()
 print(current_wd)
+# setwd
 
 # Define your DNA sequence and k-mer length
 something <- readDNAStringSet("./GCF_000837185.1/GCF_000837185.1_ViralProj14097_genomic.fna")
 list_of_files <- list.files(path = ".", recursive = TRUE, full.names = TRUE)
-print(list_of_files)
-print(something)
 
-dna_sequence <- DNAString("ATGCGTACGT")
 k <- 12
 
-empty_counts <- oligonucleotideFrequency( DNAString(""), k)
-print("empty counts")
-print(empty_counts)
-# Count oligonucleotide frequencies (k-mers)
-kmer_counts <- oligonucleotideFrequency(something, k)
-
-kmer_counts<-  kmer_counts + oligonucleotideFrequency(something, k)
-
-print(kmer_counts)
+total_counts <- oligonucleotideFrequency( DNAString(""), k)
+for (file in list_of_files){
+  # print(typeof(file))
+  if(endsWith(file, ".fna")){
+    print(file)
+    dnass <- readDNAStringSet(file)
+    total_counts <- total_counts + oligonucleotideFrequency(dnass[[1]], k)
+  }
+}
+    
+# print(kmer_counts)
+print(total_counts)
