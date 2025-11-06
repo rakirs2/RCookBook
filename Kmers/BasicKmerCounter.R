ec6d77112fc4106d4c2f0aefac662b473570d6b1
Srikar <srikarmylavarapu@gmail.com>
2025-11-05 21:08:25 -0600

    simple counter for single file

--- Kmers/BasicKmerCounter.R
+++ Kmers/BasicKmerCounter.R
@@ -1,18 +1,3 @@
-# install.packages("kmer")
-# library(kmer)
-# 
-# # Example DNA sequence
-# sequence <- "ATCGATCAC"
-# 
-# # Define k-mer length
-# k <- 3
-# 
-# # Count k-mers
-# kmer_counts <- kcount(sequence, k = k)
-# 
-# # View the k-mer counts
-# print(kmer_counts)
-
 # Install Biostrings if not already installed
 if (!requireNamespace("BiocManager", quietly = TRUE))
 install.packages("BiocManager")
@@ -21,18 +6,22 @@ library(Biostrings)
 
 current_wd <- getwd()
 print(current_wd)
-# setwd("./Downloads/ncbi_dataset/ncbi_dataset/data")
-# current_wd <- getwd()
-# print(current_wd)
 
 # Define your DNA sequence and k-mer length
 something <- readDNAStringSet("./GCF_000837185.1/GCF_000837185.1_ViralProj14097_genomic.fna")
 list_of_files <- list.files(path = ".", recursive = TRUE, full.names = TRUE)
 print(list_of_files)
 print(something)
-dna_sequence <- DNAString("ATGCGTACGT")
-k <- 3
 
+dna_sequence <- DNAString("ATGCGTACGT")
+k <- 12
+
+empty_counts <- oligonucleotideFrequency( DNAString(""), k)
+print("empty counts")
+print(empty_counts)
 # Count oligonucleotide frequencies (k-mers)
-kmer_counts <- oligonucleotideFrequency(dna_sequence, k)
+kmer_counts <- oligonucleotideFrequency(something, k)
+
+kmer_counts<-  kmer_counts + oligonucleotideFrequency(something, k)
+
 print(kmer_counts)
